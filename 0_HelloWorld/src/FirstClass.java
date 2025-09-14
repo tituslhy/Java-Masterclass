f09b5322cf66ee2694322e3ddb8da7c804834ca6
Titus Lim <tituslhy@gmail.com>
2025-09-14 13:49:45 +0800

    add modifier

--- 0_HelloWorld/src/FirstClass.java
+++ 0_HelloWorld/src/FirstClass.java
@@ -1,3 +1,6 @@
+//public is an access modifier - enabling full access to the class or method
+//`main` is a special entry point method that Java always looks for when running
+
 public class FirstClass {
     public static void main(String[] args) {
         System.out.print("Hello World");
