ce3d474196ae1c5e87ab2ce05a3e3f406f1039d7
Titus Lim <tituslhy@gmail.com>
2025-09-30 13:28:52 +0800

    created first package

---
+++ 4_Advanced_OOP/Packages/src/dev/lpa/Main.java
@@ -0,0 +1,10 @@
+package dev.lpa;
+
+import dev.lpa.com.abc.first.Item;
+
+public class Main {
+    public static void main(String[] args) {
+        Item firstItem = new Item("Burger");
+        System.out.println(firstItem);
+    }
+}
