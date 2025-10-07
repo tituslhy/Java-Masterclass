dafb2eedf60b70b37e08668add7155b5c5d5c2b5
Titus Lim <tituslhy@gmail.com>
2025-10-07 09:58:47 +0800

    add new array list methods

--- 6_Lists_Iterators_AutoBoxing/src/dev/JavaMasterclass/Main.java
+++ 6_Lists_Iterators_AutoBoxing/src/dev/JavaMasterclass/Main.java
@@ -2,6 +2,7 @@ package dev.JavaMasterclass;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /*
 Remember records have their own default constructors
@@ -9,6 +10,11 @@ Remember records have their own default constructors
 record GroceryItem(String name, String type, int count){
     public GroceryItem(String name){
         this(name, "DAIRY", 1);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d %s in %s", count, name.toUpperCase(), type);
     }
 }
 
@@ -30,6 +36,29 @@ public class Main {
         ArrayList<GroceryItem> groceryList = new ArrayList<>();
         groceryList.add(new GroceryItem("Butter"));
         groceryList.add(new GroceryItem("Yoghurt"));
+        groceryList.add(0, new GroceryItem("Milk")); //overloaded add method takes in an index to insert
+        groceryList.set(0, new GroceryItem("Apples")); //overloaded add method takes in an index to insert
         System.out.println(groceryList); //Default print statements are supported
+        groceryList.remove(1);
+        System.out.println(groceryList);
+
+        String[] items = {"apples", "bananas", "milk", "eggs"};
+        List<String> list = List.of(items); //factorymethod. Note that this is not an arrayList
+        System.out.println(list);
+        System.out.println(list.getClass().getName()); //Immutable nested class. Cannot add/remove/reorder.
+
+        // Creating a new mutable list from an immutable list
+        ArrayList<String> groceries = new ArrayList<>(list);
+        groceries.add("yoghurt");
+        System.out.println(groceries);
+
+        ArrayList<String> nextList = new ArrayList<>(
+                List.of("pickles","mustard","cheese")
+        );
+        System.out.println(nextList);
+
+        //Add lists
+        groceries.addAll(nextList);
+        System.out.println(groceries);
     }
 }
