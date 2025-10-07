c851473a0fdc97260bd43548f65ec7fc506c032e
Titus Lim <tituslhy@gmail.com>
2025-10-07 10:52:32 +0800

    complete array challenge

---
+++ 6_Lists_Iterators_AutoBoxing/ArrayChallenge/src/Main.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> groceryList = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        int action = 1;
+        do {
+            System.out.println(
+                    "Choose:" +
+                            "\n-0 to shutdown the program, "+
+                            "\n-1 to add items to grocery list, "+
+                            "\n-2 to remove items from grocery list"
+            );
+            action = Integer.parseInt(scanner.nextLine());
+            System.out.println("Received action: " + action);
+            if ((action>2) || (action<0)){
+                System.out.println("Only enter 0, 1, or 2");
+            } else if (action==1){
+                System.out.println("Add items (separated by commas) to grocery list: ");
+                String groceries = scanner.nextLine();
+                String[] parts = groceries.split(",");
+                for(String part : parts){
+                    if (groceryList.contains(part)){
+                        System.out.println("Skipping " + part + " because it is already in the grocery list");
+                        continue;
+                    } groceryList.add(part);
+                }
+                System.out.println("Updated grocery list: " + groceryList);
+            } else if (action==2){
+                if (groceryList.isEmpty()){
+                    System.out.println("No element left in the grocery list to remove");
+                    continue;
+                }
+                System.out.println("Add items (separated by commas) to grocery list: ");
+                String groceries = scanner.nextLine();
+                String[] parts = groceries.split(",");
+                for(String part : parts){
+                    groceryList.remove(part);
+                }
+                System.out.println("Updated grocery list: " + groceryList);
+            }
+        } while(action != 0);
+        System.out.println("Shutting down grocery list program. Bye!");
+    }
+}
+
+//0 - shutdown
+//1 - add item(s) to list (comma delimited)
+//2 - remove any items (commma delimited list)
+
+
